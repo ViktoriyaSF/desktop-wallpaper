6122ba49f33fe081abea687514a9512c0bfb1c94
ViktoriyaSF <kievstar01051985@gmail.com>
2023-03-31 00:33:32 +0300

    not

--- src/index.js
+++ src/index.js
@@ -169,47 +169,39 @@ document.querySelector('.tumbler-wrapper').addEventListener('click', () => {
   galleryPhoto.classList.toggle('night-mode');
 });
 // ----------------
+const container = document.getElementById('tui-pagination-container');
+const options = {
+  // below default value of options
+  totalItems: 10,
+  itemsPerPage: 10,
+  visiblePages: 10,
+  page: 1,
+  centerAlign: false,
+  firstItemClassName: 'tui-first-child',
+  lastItemClassName: 'tui-last-child',
+  template: {
+    page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+    currentPage:
+      '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+    moveButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</a>',
+    disabledMoveButton:
+      '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</span>',
+    moreButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+      '<span class="tui-ico-ellip">...</span>' +
+      '</a>',
+  },
+};
+const pagination = new Pagination(container, options);
+console.log(pagination);
+pagination.reset();
+
 // const container = document.getElementById('tui-pagination-container');
-// const options = {
-//   // below default value of options
-//   totalItems: 10,
-//   itemsPerPage: 10,
-//   visiblePages: 10,
-//   page: 1,
-//   centerAlign: false,
-//   firstItemClassName: 'tui-first-child',
-//   lastItemClassName: 'tui-last-child',
-//   template: {
-//     page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-//     currentPage:
-//       '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-//     moveButton:
-//       '<a href="#" class="tui-page-btn tui-{{type}}">' +
-//       '<span class="tui-ico-{{type}}">{{type}}</span>' +
-//       '</a>',
-//     disabledMoveButton:
-//       '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
-//       '<span class="tui-ico-{{type}}">{{type}}</span>' +
-//       '</span>',
-//     moreButton:
-//       '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
-//       '<span class="tui-ico-ellip">...</span>' +
-//       '</a>',
-//   },
-// };
-// const pagination = new Pagination(container, options);
-// console.log(pagination);
-// pagination.reset();
+// const instance = new Pagination(container, { options });
 
-const pagination1 = new tui.Pagination(document.getElementById('pagination1'), {
-  totalItems: 500,
-  itemsPerPage: 10,
-  visiblePages: 5,
-});
-
-const pagination2 = new tui.Pagination(document.getElementById('pagination2'), {
-  totalItems: 500,
-  itemsPerPage: 10,
-  visiblePages: 5,
-  centerAlign: true,
-});
+//
