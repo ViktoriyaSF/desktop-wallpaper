dd833d2019345db1231c599fb93e14d0a7bf22ad
ViktoriyaSF <kievstar01051985@gmail.com>
2023-03-31 22:10:41 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import './css/styles.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import axios from 'axios';
-import Pagination from 'tui-pagination';
 
 // Описаний в документації
 import SimpleLightbox from 'simplelightbox';
@@ -169,39 +168,3 @@ document.querySelector('.tumbler-wrapper').addEventListener('click', () => {
   galleryPhoto.classList.toggle('night-mode');
 });
 // ----------------
-const container = document.getElementById('tui-pagination-container');
-const options = {
-  // below default value of options
-  totalItems: 10,
-  itemsPerPage: 10,
-  visiblePages: 10,
-  page: 1,
-  centerAlign: false,
-  firstItemClassName: 'tui-first-child',
-  lastItemClassName: 'tui-last-child',
-  template: {
-    page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-    currentPage:
-      '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-    moveButton:
-      '<a href="#" class="tui-page-btn tui-{{type}}">' +
-      '<span class="tui-ico-{{type}}">{{type}}</span>' +
-      '</a>',
-    disabledMoveButton:
-      '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
-      '<span class="tui-ico-{{type}}">{{type}}</span>' +
-      '</span>',
-    moreButton:
-      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
-      '<span class="tui-ico-ellip">...</span>' +
-      '</a>',
-  },
-};
-const pagination = new Pagination(container, options);
-console.log(pagination);
-pagination.reset();
-
-// const container = document.getElementById('tui-pagination-container');
-// const instance = new Pagination(container, { options });
-
-//
